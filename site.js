d36e951238c87dc10e0f66b76e55e2a5c906d558
Fizza Arif <farif@hawk.iit.edu>
2019-02-07 22:01:43 -0600

    Clean up the number by removing any keys that are not numbers and also removing the 1st number(1) if theres more than 10 numbers

--- site.js
+++ site.js
@@ -11,5 +11,11 @@
   tel_input.addEventListener('focus', function(){
     console.log('The label is in focus');
   });
+  tel_input.addEventListener('keyup', function(){
+    console.log('The value of #telephone is', this.value);
+    var clean_number = this.value.replace(/\D/g,'');
+    var actual_number = clean_number.replace(/^1/,'');
+    
+  });
 
 }());
