11673a3fa349f850f454c22ab08b1e4a4c52e417
Fizza Arif <farif@hawk.iit.edu>
2019-02-27 19:48:45 -0600

    Add lowerlevel function named equal

--- site.js
+++ site.js
@@ -3,24 +3,6 @@
     return;
   }
 
-  var submit = document.querySelector('#submit');
-
-  document.addEventListener('DOMContentLoaded', function(){
-    submit.setAttribute('disabled', 'disabled');
-  });
-
-  var tel_input = document.querySelector('#telephone');
-  tel_input.addEventListener('focus', function(){
-    console.log('The label is in focus');
-  });
-  tel_input.addEventListener('keyup', function(){
-    console.log('The value of #telephone is', this.value);
-    var clean_number = this.value.replace(/\D/g,'');
-    var actual_number = clean_number.replace(/^1/,'');
-    if (actual_number.length === 10) {
-      submit.removeAttribute('disabled');
-    }
-
-  });
-
-}());
+  function eq(value,condition) {
+    return value === condition;
+  }
