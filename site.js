fa9f2f2bbfed6dc059d6c7be858d666dd62d0d0c
Fizza Arif <farif@hawk.iit.edu>
2019-02-27 19:51:01 -0600

    Add validate function

--- site.js
+++ site.js
@@ -10,3 +10,13 @@
   function whitespace(value) {
     return value.replace(/\s/g,'');
   }
+
+  function validate(value,check,condition) {
+    if (eq(typeof(check.test),'function')) {
+      return check.test(value);
+    } else if (eq(typeof(check),'function')) {
+        return check(value,condition);
+    } else {
+        return false;
+    }
+  }
